4e5d3688828200dede3a32c7188c064a370e7909
Git69Guy <82217254+Git69Guy@users.noreply.github.com>
2021-04-09 20:28:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+// var 
+// a=10;
+
+// while (a<=20)
+// {
+// document.write("<br>")
+// document.write(a++)
+
+// }
+// document.write("<br>")
+
+
+// var 
+// a=0;
+
+// do
+// {
+// document.write("<br>")
+// document.write(a++)
+// }
+// while (a<=10);
+
+
+
+
+// var a=2,b=4;
+
+
+// document.write("a+b")
+// function showmessage (){
+//     alert(a+b);
+// }showmessage();
+
+
+var x;
+var y;
+
+function add ( x, y){
+    var sum;
+    sum = x+y;
+    alert(sum);
+
+}
+add (4,4);
+
+
+{/* <button>show pop</button>
+showmessage() */}
+
